f74450761970c846a552a9a4bada78a94b7bd9df
niclas <niclas@NiclasLaptop.com>
2024-06-26 16:00:26 +0200

    add NaviBar function and HomeView html components

--- webui/src/pages/HomeView.vue
+++ webui/src/pages/HomeView.vue
@@ -1,5 +1,35 @@
 <template>
-    <div>
-        <h1>Welcome Home!</h1>
+    <div class="container-md text-center">
+        <div class="row">
+            <p>Insert Plasmid here!</p>
+        </div>
+        <div class="row">
+            <textarea
+                v-model="input"
+                class="form-control"
+                placeholder="Insert Plasmid here!"
+                name="DNA_Input"
+                id="sequenzInput"
+            ></textarea>
+        </div>
+        <div class="row">
+            <div class="col align-self-left">
+                <input class="form-control" type="file" id="formFile" />
+            </div>
+            <div class="col align-self-right">
+                <button @click="printTest" type="button" class="btn btn-secondary">Submit</button>
+            </div>
+        </div>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const input = ref('')
+const filename = ref()
+
+function printTest() {
+    console.log(input.value)
+}
+</script>
